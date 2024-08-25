917c2034d3a17716fdb8fc0e3577fa32fc408a73
Hency Dsouza <hencyhd39@gmail.com>
2024-08-25 16:25:07 +0530

    feat: add auth on dashboard

--- src/routes/v2/dashboard.route.ts
+++ src/routes/v2/dashboard.route.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express'
 // import { validate } from '../../modules/validate';
-// import authMiddleware from '../../modules/employee_auth/auth.middleware';
+import authMiddleware from '../../modules/employee_auth/auth.middleware';
 import { dashboardController } from '../../modules/dashboard';
 
 const router: Router = express.Router();
 
 router.route('/')
-    .get(dashboardController.dashboard)
-    // .get(authMiddleware(['product', 'company', 'building']), dashboardController.dashboard)
+    // .get(dashboardController.dashboard)
+    .get(authMiddleware(['product', 'company', 'building']), dashboardController.dashboard)
 
 export default router
